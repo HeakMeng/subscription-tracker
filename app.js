418b2ce2c77a5be9363ae8eb050cc5abc2588cfa
HeakKnight <heakmeng42@gmail.com>
2025-11-19 00:33:07 +0700

    made routes

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 import express from 'express';
+
 import { PORT } from './config/env.js';
+import authRouter  from './routes/auth.routes.js';
+import userRouter  from './routes/user.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("=> Home-Page <=")
-})
-
-app.get("/register", (req, res) => {
-  res.send(" => Let's register <=");
-})
+app.use('/', authRouter);
+app.use('/', userRouter);
+app.use('/', subscriptionRouter);
 
 app.listen(PORT, () => {
-  console.log("=> App listening on port",PORT);
+  console.log(`=> App listening on port",${PORT}👊`);
 })  
 
 export default app;
---
+++ routes/user.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const userRouter = Router();
+
+userRouter.get('/', (req,res) => res.send({title: "GET all users"}));
+userRouter.get('/:id', (req,res) => res.send({title: "GET specific user!"}));
+userRouter.post('/', (req,res) => res.send({title: "CREATE new user"}));
+userRouter.get('/:id', (req,res) => res.send({title: "UPDATE all users"}));
+userRouter.delete('/:id', (req,res) => res.send({title: "DELETE user"}));
+
+export default userRouter;
