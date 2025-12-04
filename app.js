769590f4a54029724e8367480ea81d8e0b973c00
HeakMeng <phengmengheak@outlook.com>
2025-12-04 22:11:48 +0700

    Created middleware for handling error

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import { PORT } from './config/env.js';
 import authRouter  from './routes/auth.routes.js';
 import userRouter  from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errrorMiddleware from './middleware/error.middleware.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errrorMiddleware);
 
 app.listen(PORT, async () => {
   console.log(`=> App listening on port",${PORT}👊`);
